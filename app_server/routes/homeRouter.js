6de76b2411bb3002531fe6814b2ac6f87e91613b
Hrushikesh <hrushikesh1330@gmail.com>
2024-07-16 18:21:32 +0000

    minor fixes

--- app_server/routes/homeRouter.js
+++ app_server/routes/homeRouter.js
@@ -4,6 +4,6 @@ const homeC = require("../controllers/homeController");
 
 router.get("/", homeC.hRender);
 router.get("/data", homeC.hdata);
-router.get("/data", homeC.hdata);
+router.get("/about", homeC.hdata);
 
 module.exports =  router
