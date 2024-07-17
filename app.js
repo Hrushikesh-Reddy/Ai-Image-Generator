22a0d3c83a964fb376f3d91b4ff26923a6882a48
Hrushikesh <hrushikesh1330@gmail.com>
2024-07-17 11:09:22 +0530

    added bin

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan")
+
 const app = express();
-const port = process.env.PORT || 4000;
 
 const genrouter = require('./app_server/routes/genRouter')
 const homeRouter = require("./app_server/routes/homeRouter")
@@ -14,4 +14,4 @@ app.use(express.static(__dirname));
 app.use('/', homeRouter)
 app.use('/genpage', genrouter)
 
-app.listen(port, () => {console.log(`Listening on port ${port}...`)});
+module.exports = app;
